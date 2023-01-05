f568a122441f09b313e057890e5eec2f83ae0e34
basharjamal <121374162+basharjamal@users.noreply.github.com>
2023-01-05 16:32:03 +0300

    fourth question 2

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 
 
 
-tuple1=(10,20,30,40,50)
-array=[]
-for i in range(0,len(tuple1)):
- array.append(tuple1[i])
-
-array.reverse()
-tuple1=tuple(array)
-
-print(tuple1)
+tuple1=("Orange",[10,20,30],(5,15,25))
+item=tuple1[1][1]
+print(item)
 
 
 
