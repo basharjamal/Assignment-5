72edff09496219a500c52dfaf0e3ec5ed1dbe157
basharjamal <121374162+basharjamal@users.noreply.github.com>
2023-01-05 16:01:40 +0300

    set 3 question 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
-sample_set={"Yellow","Orange","Black"}
-sample_list=["Blue","Green","red"]
-sample_union=sample_set.union(sample_list)
-print(sample_union)
+
+set1={10,20,30,40,50}
+set2={30,40,50,60,70}
+set3=set1.intersection(set2)
+print(set3)
