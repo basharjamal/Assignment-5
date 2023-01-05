06a671e3140863ad0cff5e2b5f91fc7d10ea07e1
basharjamal <121374162+basharjamal@users.noreply.github.com>
2023-01-05 16:44:21 +0300

    fourth question 3

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 
 
 
-tuple1=("Orange",[10,20,30],(5,15,25))
-item=tuple1[1][1]
-print(item)
-
-
-
+tuple1=(10,20,30,40)
+print(tuple1)
+print("# Your code")
+char="a"
+for i in range(0,len(tuple1)):
+    if i==0:
+        char="a"
+    elif i==1:
+        char="b"
+    elif i==2:
+        char="c"
+    elif i==3:
+        char="d"
+    print("print(",char,") # should print ",tuple1[i])
 
 
