b070edef5a86b758e8d932b6748398f063fa577e
basharjamal <121374162+basharjamal@users.noreply.github.com>
2023-01-05 13:15:03 +0300

    direction 2 question 1 (modified2)

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 
 keys=["Ten","Twenty","Thirty"]
 values=[10,20,30]
+dict_array:dict={}
 for i in range(0,len(keys)):
- dict_array={
-    keys[i] :values[i]
- }
+ dict_array[keys[i]] =values[i]
 print(dict_array)
